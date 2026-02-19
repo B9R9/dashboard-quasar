ec571ebbe76311445633ffe9e225b1b7950ac258
Baptiste <baptiste.riffard@gmail.com>
2026-02-19 14:32:58 +0200

    Add route for Settings page in router configuration

--- react-app/src/router/router.jsx
+++ react-app/src/router/router.jsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Home from '../pages/Home'
+import Settings from '../pages/Settings'
 
-const router = createBrowserRouter([{ path: '/', element: <Home /> }])
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/settings', element: <Settings /> },
+])
 
 export default router
