c77293df54bfd9767b79c4315bc44271a891ccb1
Baptiste <baptiste.riffard@gmail.com>
2026-02-25 10:18:39 +0200

    fix: update SearchInput to pass the entire event object to onChange handler

--- react-app/src/modules/search/components/SearchInput.jsx
+++ react-app/src/modules/search/components/SearchInput.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function SearchInput({ value, onChange, onClear }) {
   const handleInputChange = (event) => {
-    onChange(event.target.value)
+    onChange(event)
   }
 
   return (
